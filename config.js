c3cb5aace5ce6e71f5a33bb931143ad4b5d9837b
WebEskuvo <kukinuni42069@gmail.com>
2026-05-28 08:17:12 +0300

    Update Worker URL to production endpoint

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Konfigurációs fájl
 const CONFIG = {
-  WORKER_URL: "https://eskuvo-upload.gergely-botond.workers.dev",
+  WORKER_URL: "https://eskuvo-upload-production.gergely-botond.workers.dev",
   DRIVE_FOLDER_ID: "DRIVE_FOLDER_ID_PLACEHOLDER",
   ADMIN_PASSWORD_HASH: "ADMIN_HASH_PLACEHOLDER"
 };
