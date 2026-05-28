5f5faffb443d9ad5898e2bb54b7e9e1e67af2339
WebEskuvo <kukinuni42069@gmail.com>
2026-05-28 08:45:21 +0300

    Fix config.js with admin password hash

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Konfigurációs fájl
 const CONFIG = {
   WORKER_URL: "https://eskuvo-upload-production.gergely-botond.workers.dev",
-  DRIVE_FOLDER_ID: "DRIVE_FOLDER_ID_PLACEHOLDER",
-  ADMIN_PASSWORD_HASH: "ADMIN_HASH_PLACEHOLDER"
+  DRIVE_FOLDER_ID: "1CcpN3MUtGl6OS49gH7_9mE4nHn4vX1ea",
+  ADMIN_PASSWORD_HASH: "2cb815be3f1f2e076de7c9efbc0718ad119cbc1c0db2553c7f4a5212d38c8298"
 };
